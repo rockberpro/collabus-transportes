7ff8f0c22c2220bdc246ed4c471e1cdba863a0f0
Rockberpro <rockberpro@gmail.com>
2025-08-09 19:09:50 -0300

    feat: Install Nuxt
    Nuxt4 and NuxtUI Pro

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  devServer: {
+    port: 3000,
+    host: '0.0.0.0',
+    https: false
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui-pro'
+  ],
+  css: ['~/assets/css/main.css']
+})
