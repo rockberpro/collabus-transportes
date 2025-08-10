816cae01707b172156e96947de7f232ab3ce6d33
Rockberpro <rockberpro@gmail.com>
2025-08-10 02:03:03 -0300

    fix: Error rendering Dashboard

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <UDashboardPage>
-    <UDashboardPanel>
-      <UDashboardNavbar>
-        <template #toggle>
-          <UIcon name="mdi:menu" :size="20"/>
-        </template>
-        <template #leading>
-          <UBadge label="Collabus" size="xl" variant="subtle" />
-        </template>
-        <template #right>
-          <ColorModeButton />
-        </template>
-      </UDashboardNavbar>
-    </UDashboardPanel>
-  </UDashboardPage>
+  <UDashboardPanel>
+    <UDashboardNavbar>
+      <template #toggle>
+        <UIcon name="mdi:menu" :size="20"/>
+      </template>
+      <template #leading>
+        <UBadge label="Collabus" size="xl" variant="subtle" />
+      </template>
+      <template #right>
+        <ColorModeButton />
+      </template>
+    </UDashboardNavbar>
+  </UDashboardPanel>
 </template>
