6ee18d1d47916949078acec9267d8a5d5a4db880
Rockberpro <rockberpro@gmail.com>
2025-09-14 20:20:29 -0300

    feat: make home badge clicable

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,23 @@
         <UIcon name="mdi:menu" :size="20" />
       </template>
       <template #leading>
-        <UBadge label="Collabus" size="xl" variant="subtle" />
+        <UBadge 
+          size="xl" 
+          variant="subtle" 
+          @click="navigateTo('/')"
+          class="cursor-pointer hover:opacity-75 transition-opacity"
+        >
+          <template #default>
+            <div class="flex items-center gap-2">
+              <img 
+                src="~/assets/images/logo.png" 
+                alt="Logo" 
+                class="h-10 w-auto"
+              />
+              <span>Collabus</span>
+            </div>
+          </template>
+        </UBadge>
       </template>
       <template #right>
         <ColorModeButton />
