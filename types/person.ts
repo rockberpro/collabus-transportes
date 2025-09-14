9d0cd8046ea4ba20fc78e5708eeea25c9eca165d
Rockberpro <rockberpro@gmail.com>
2025-09-14 01:04:52 -0300

    chore: remove unused mappings

--- types/person.ts
+++ types/person.ts
@@ -32,26 +32,6 @@ export interface CreatePersonData {
   userId: ObjectId;
 }
 
-// Funções utilitárias para mapeamento
-export const mapPersonDocumentToPerson = (doc: PersonDocument): Person => ({
-  id: doc._id?.toString(),
-  name: doc.name,
-  userId: doc.userId,
-  createdAt: doc.createdAt,
-  updatedAt: doc.updatedAt,
-});
-
-export const mapCreatePersonDataToPersonDocument = (
-  data: CreatePersonData
-): CreatePersonDocument => {
-  return {
-    name: data.name,
-    userId: data.userId,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-};
-
 // Interface para person com informações do usuário
 export interface PersonWithUser {
   id?: string;
