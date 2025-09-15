9651920e7ccc6b4e9fb061214ee72ad0062fbf67
Rockberpro <rockberpro@gmail.com>
2025-09-15 20:27:41 -0300

    fix: logo diplay on navbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <UDashboardPanel class="py-5">
-    <UDashboardNavbar>
+    <UDashboardNavbar class="pb-10 pt-10">
       <template #toggle>
         <UIcon name="mdi:menu" :size="20" />
       </template>
@@ -9,15 +9,15 @@
           size="xl" 
           variant="subtle" 
           @click="navigateTo('/')"
-          class="cursor-pointer hover:opacity-75 transition-opacity"
+          class="cursor-pointer hover:opacity-75 transition-opacity py-4"
         >
           <template #default>
             <div class="flex items-center gap-2">
-              <img 
+              <!-- <img 
                 src="~/assets/images/logo.png" 
                 alt="Logo" 
                 class="h-10 w-auto"
-              />
+              /> -->
               <span>Collabus</span>
             </div>
           </template>
